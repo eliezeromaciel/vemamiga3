bdbb4d2bb924182b640d8881a44e9ffdd2b586ba
eliezer Maciel <eliezeromaciel@gmail.com>
2023-07-26 11:10:45 -0300

    cria rotas parametros, empresas e clientes. conserta sidebar, para fecha-lo quando nova rota é acessada.

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -7,7 +7,7 @@
                     <span class="ms-2 d-sm-inline " >Cadastros</span>
                 </a>
                 <ul class="nav collapse ms-1 flex-column" id="sidemenu" data-bs-parent="#menu"> 
-                    <li class="nav-item my-1">
+                    <li class="nav-item my-1" data-bs-toggle="offcanvas">
                         <a class="nav-link text-white ms-3" href="/empresas" aria-current="page">
                             <i class="bi bi-pie-chart"></i>
                             Empresas
@@ -22,7 +22,7 @@
                     <span class="ms-2 d-sm-inline">Configurações</span>
                 </a>
                 <ul class="nav collapse ms-1 flex-column " id="sidemenu2" data-bs-parent="#menu"> 
-                    <li class="nav-item my-1">
+                    <li class="nav-item my-1" data-bs-toggle="offcanvas">
                         <a class="nav-link text-white ms-3" href="/clientes" aria-current="page">
                             <i class="bi bi-people"></i>
                             Clientes
@@ -30,15 +30,15 @@
                     </li>
                 </ul>
                 <ul class="nav collapse ms-1 flex-column" id="sidemenu2" data-bs-parent="#menu"> 
-                    <li class="nav-item my-1">
-                        <a class="nav-link text-white ms-3" href="/#" aria-current="page">
-                            <i class="bi bi-gear"></i>
+                    <li class="nav-item my-1" data-bs-toggle="offcanvas">
+                        <a class="nav-link text-white ms-3"  href="/parametros" aria-current="page">
+                            <i class="bi bi-gear" ></i>
                             Parâmetros
                         </a>
                     </li>
                 </ul>
                 <ul class="nav collapse ms-1 flex-column" id="sidemenu2" data-bs-parent="#menu"> 
-                    <li class="nav-item my-1">
+                    <li class="nav-item my-1" data-bs-toggle="offcanvas">
                         <a class="nav-link text-white ms-3" href="/#" aria-current="page">
                             <i class="bi bi-question-circle"></i>
                             Seila
---
+++ src/routes/(app)/clientes/+page.svelte
@@ -0,0 +1,1 @@
+<h1>página CLIENTES</h1>
